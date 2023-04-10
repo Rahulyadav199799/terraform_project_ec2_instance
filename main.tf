85a490e2a62d6bcf7f016771e39785ab2e558a2a
Rahulyadav199799 <100790896+Rahulyadav199799@users.noreply.github.com>
2023-04-10 20:43:07 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+#To create an Ec2 Instance in us-east-1
+resource "aws_instance" "server1" {
+  ami           = "ami-0fa1de1d60de6a97e" #us-east-1
+  instance_type = "t2.micro"
+  key_name      = "Rahulkey123"
